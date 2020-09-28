e93fe9bd23760c828b0345ffed6fb9f5499fe819
mlubneuskaya <mlubnewskaja@gmail.com>
2020-09-28 22:23:35 +0300

    new class Waiter for parsing command line arguments

--- src/com/breakfast/Main.java
+++ src/com/breakfast/Main.java
@@ -1,34 +1,11 @@
 package com.breakfast;
 
-
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Food> breakfast = new ArrayList<>();
-        for (String argument : args) {
-            String[] parameters = argument.split("/");
-            Class<?> someClass;
-            try {
-                someClass = Class.forName("com.breakfast." + parameters[0]);
-                if(parameters.length == 1){
-                    Constructor<?> constructor = someClass.getConstructor();
-                    breakfast.add((Food)constructor.newInstance());
-                } else if(parameters.length == 2){
-                    Constructor<?> constructor = someClass.getConstructor(String.class);
-                    breakfast.add((Food)constructor.newInstance(parameters[1]));
-                }else{
-                    Constructor<?> constructor = someClass.getConstructor(String.class, String.class);
-                    breakfast.add((Food)constructor.newInstance(parameters[1], parameters[2]));
-                }
-            } catch (ClassNotFoundException | NoSuchMethodException |
-                    InstantiationException | IllegalAccessException | InvocationTargetException exception) {
-                exception.printStackTrace();
-            }
-        }
+        Waiter waiter = new Waiter();
+        List<Food> breakfast = waiter.getBreakfast(args);
         for(Food piece: breakfast){
             System.out.println(piece.toString());
         }
---
+++ src/com/breakfast/Waiter.java
@@ -0,0 +1,33 @@
+package com.breakfast;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Waiter {
+    public List<Food> getBreakfast(String[] arguments){
+        List<Food> breakfast = new ArrayList<>();
+        for (String argument : arguments) {
+            String[] parameters = argument.split("/");
+            Class<?> someClass;
+            try {
+                someClass = Class.forName("com.breakfast." + parameters[0]);
+                if(parameters.length == 1){
+                    Constructor<?> constructor = someClass.getConstructor();
+                    breakfast.add((Food)constructor.newInstance());
+                } else if(parameters.length == 2){
+                    Constructor<?> constructor = someClass.getConstructor(String.class);
+                    breakfast.add((Food)constructor.newInstance(parameters[1]));
+                }else{
+                    Constructor<?> constructor = someClass.getConstructor(String.class, String.class);
+                    breakfast.add((Food)constructor.newInstance(parameters[1], parameters[2]));
+                }
+            } catch (ClassNotFoundException | NoSuchMethodException |
+                    InstantiationException | IllegalAccessException | InvocationTargetException exception) {
+                exception.printStackTrace();
+            }
+        }
+        return breakfast;
+    }
+}
