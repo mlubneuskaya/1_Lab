8823054e10019ead606533c34fd00ecdc12da411
mlubneuskaya <mlubnewskaja@gmail.com>
2020-09-28 14:27:08 +0300

    Sandwich class implementation

--- src/com/breakfast/Sandwich.java
+++ src/com/breakfast/Sandwich.java
@@ -4,7 +4,24 @@ import static com.breakfast.FoodType.SANDWICH;
 
 public class Sandwich extends Food {
 
-    public Sandwich(FoodType type) {
-        super(type);
+    private String filling1;
+    private String filling2;
+
+    public Sandwich(String filling1, String filling2){
+        super(SANDWICH);
+        this.filling1 = filling1;
+        this.filling2 = filling2;
+    }
+    public String getFilling1(){
+        return filling1;
+    }
+    public String getFilling2(){
+        return filling2;
+    }
+    public void setFilling1(String filling1){
+        this.filling1 = filling1;
+    }
+    public void setFilling2(String filing2){
+        this.filling2 = filling2;
     }
 }
